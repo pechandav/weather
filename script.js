4ce6aed1630f4d212dea1c3b66c44ed36a7b75ba
Pechanda <pechandav@gmail.com>
2020-07-23 21:54:23 -0700

    adding openweathe api/testing

--- script.js
+++ script.js
@@ -1,29 +1,6 @@
-var key = "YOUR KEY";
-var city = "YOUR CITY"; // My test case was "London"
-var url = "https://api.openweathermap.org/data/2.5/forecast";
+const CURRENT_LOCATION = document.getElementsByClassName('weather-content__overview')[0];
+const CURRENT_TEMP = document.getElementsByClassName('weather-content__temp')[0];
+const FORECAST = document.getElementsByClassName('component__forecast-box')[0];
 
-$.ajax({
-  url: url, //API Call
-  dataType: "json",
-  type: "GET",
-  data: {
-    q: city,
-    appid: key,
-    units: "metric",
-    cnt: "10"
-  },
-  success: function(data) {
-    console.log('Received data:', data) // For testing
-    var wf = "";
-    wf += "<h2>" + data.city.name + "</h2>"; // City (displays once)
-    $.each(data.list, function(index, val) {
-      wf += "<p>" // Opening paragraph tag
-      wf += "<b>Day " + index + "</b>: " // Day
-      wf += val.main.temp + "&degC" // Temperature
-      wf += "<span> | " + val.weather[0].description + "</span>"; // Description
-      wf += "<img src='https://openweathermap.org/img/w/" + val.weather[0].icon + ".png'>" // Icon
-      wf += "</p>" // Closing paragraph tag
-    });
-    $("#showWeatherForcast").html(wf);
-  }
-});
+const URL = "https://api.openweathermap.org/data/2.5/forecast/daily?" +
+"q=CITY&cnt=7&units=imperial&APPID=YOUR_APP_ID";
