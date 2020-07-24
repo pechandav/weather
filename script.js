0f2cd47a97bf5fd6bb3cad80d57625ae700f9c7e
Pechanda <pechandav@gmail.com>
2020-07-24 00:57:09 -0700

    trying to change celcius to faranheit

--- script.js
+++ script.js
@@ -1,136 +1,89 @@
-var x = document.querySelector(".city-name");
-var date = new Date();
-var hour = date.getHours();
+const iconElement = document.querySelector(".weather-icon");
+const tempElement = document.querySelector(".temperature-value p");
+const descElement = document.querySelector(".temperature-description p");
+const locationElement = document.querySelector(".location p");
+const notificationElement = document.querySelector(".notification");
 
+// App data
+const weather = {};
 
-// Event handling for buttons "Check" 
-document.getElementById("btn-location").addEventListener("click", function () {
-    getLocation();
-});
-
-document.getElementById("btn-choice").addEventListener("click", function () {
-    getWheater($("#city-input").val());
-});
-
-// Event handling for press Enter 
-document.getElementById("city-input").addEventListener("keypress", function (event) {
-    if (event.keyCode == 13) {
-        getWheater($("#city-input").val());
-        console.log("asd");
-        $("#city-input").val("");
-    }
-}, false);
-
-
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition, showError);
-    } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
-    }
+weather.temperature = {
+    unit : "celsius"
 }
 
-function showPosition(position) {
-    lat = position.coords.latitude;
-    lon = position.coords.longitude;
-    displayLocation(lat, lon);
+// APP CONSTS AND VARS
+const KELVIN = 273;
+// API KEY
+const key = "82005d27a116c2880c8f0fcb866998a0";
+
+// CHECK IF BROWSER SUPPORTS GEOLOCATION
+if('geolocation' in navigator){
+    navigator.geolocation.getCurrentPosition(setPosition, showError);
+}else{
+    notificationElement.style.display = "block";
+    notificationElement.innerHTML = "<p>Browser doesn't Support Geolocation</p>";
 }
 
-function showError(error) {
-    switch (error.code) {
-        case error.PERMISSION_DENIED:
-            x.innerHTML = "User denied the request for Geolocation."
-            break;
-        case error.POSITION_UNAVAILABLE:
-            x.innerHTML = "Location information is unavailable."
-            break;
-        case error.TIMEOUT:
-            x.innerHTML = "The request to get user location timed out."
-            break;
-        case error.UNKNOWN_ERROR:
-            x.innerHTML = "An unknown error occurred."
-            break;
-    }
+// SET USER'S POSITION
+function setPosition(position){
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    
+    getWeather(latitude, longitude);
 }
 
-function displayLocation(latitude, longitude) {
-    var geocoder;
-    geocoder = new google.maps.Geocoder();
-    var latlng = new google.maps.LatLng(latitude, longitude);
-
-    geocoder.geocode({
-            'latLng': latlng
-        },
-        function (results, status) {
-            if (status == google.maps.GeocoderStatus.OK) {
-                if (results[0]) {
-                    var add = results[0].formatted_address;
-                    var value = add.split(",");
-
-                    count = value.length;
-                    country = value[count - 1];
-                    city = value[count - 2];
-                    adress = value[count - 3];
-                    correct = true;
-                    x.innerHTML = city;
-                    getWheater(city);
-                } else {
-                    x.innerHTML = "address not found";
-                }
-            } else {
-                x.innerHTML = "Geocoder failed due to: " + status;
-            }
-        }
-    );
+// SHOW ERROR WHEN THERE IS AN ISSUE WITH GEOLOCATION SERVICE
+function showError(error){
+    notificationElement.style.display = "block";
+    notificationElement.innerHTML = `<p> ${error.message} </p>`;
 }
 
-function getWheater(city) {
-    $.getJSON("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric" + "&appid=3a04b53f7b6d2edbaad0c1e9b9d783f1",
-        function (response) {
-            console.log(response);
-            var city2 = city;
-            var country = response.sys.country;
-            var wheater = response.weather[0].main;
-            var temp = response.main.temp;
-            var pressure = response.main.pressure;
-            var windSpeed = response.wind.speed;
-            console.log(city2 + " " + country + " " + wheater + " " + temp + " " + pressure + " " + windSpeed);
-            wheaterSet(city2, country, wheater, temp, pressure, windSpeed);
+// GET WEATHER FROM API PROVIDER
+function getWeather(latitude, longitude){
+    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+    
+    fetch(api)
+        .then(function(response){
+            let data = response.json();
+            return data;
+        })
+        .then(function(data){
+            weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+            weather.description = data.weather[0].description;
+            weather.iconId = data.weather[0].icon;
+            weather.city = data.name;
+            weather.country = data.sys.country;
+        })
+        .then(function(){
+            displayWeather();
         });
 }
 
-function wheaterSet(city, country, wheater, temp, pressure, windSpeed) {
-    console.log(wheater);
-    if (wheater == "Clear") {
-        $("#wheater-video").html("<video autoplay muted loop " + 'id="myVideo"><source  src="videos/clearNight.mp4" type="video/mp4"></video>');
-    }
-    if (wheater == "Clear" && (hour <= 20 && hour >= 6)) {
-        $("#wheater-video").html("<video autoplay muted loop " + 'id="myVideo"><source  src="videos/clearDay.mp4" type="video/mp4"></video>');
-    }
-    if (wheater == "Rain") {
-        $("#wheater-video").html("<video autoplay muted loop " + 'id="myVideo"><source  src="videos/rain.mp4" type="video/mp4"></video>');
-    }
-    if (wheater == "Clouds") {
-        $("#wheater-video").html("<video autoplay muted loop " + 'id="myVideo"><source  src="videos/cloudsNight.mp4" type="video/mp4"></video>');
-    }
-    if (wheater == "Clouds" && (hour <= 20 && hour >= 6)) {
-        $("#wheater-video").html("<video autoplay muted loop " + 'id="myVideo"><source  src="videos/clouds.mp4" type="video/mp4"></video>');
-    }
-    if (wheater == "Snow") {
-        $("#wheater-video").html("<video autoplay muted loop " + 'id="myVideo"><source  src="videos/snow.mp4" type="video/mp4"></video>');
-    }
-    if (wheater == "Mist") {
-        $("#wheater-video").html("<video autoplay muted loop " + 'id="myVideo"><source  src="videos/mist.mp4" type="video/mp4"></video>');
-    }
-    if (wheater == "Thunderstorm") {
-        $("#wheater-video").html("<video autoplay muted loop " + 'id="myVideo"><source  src="videos/thunderstorm.mp4" type="video/mp4"></video>');
-    }
-    //    $(".container-fluid").css("background", "rgba(0, 0, 0, 0.4)");    
-    $("#city-info").html(city + " " + country);
-    $("#wheat-info").html(wheater);
-    $("#temp-info").html(temp + " &deg;C");
-    $("#pressure-info").html(pressure + " hPa");
-    $("#windSpeed-info").html(windSpeed + " m/s");
-    $("#wheater-info").show();
-    thunderstorm
+// DISPLAY WEATHER TO UI
+function displayWeather(){
+    iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
+    tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+    descElement.innerHTML = weather.description;
+    locationElement.innerHTML = `${weather.city}, ${weather.country}`;
 }
+
+// C to F conversion
+function celsiusToFahrenheit(temperature){
+    return (temperature * 9/5) + 32;
+}
+
+// WHEN THE USER CLICKS ON THE TEMPERATURE ELEMENET
+tempElement.addEventListener("click", function(){
+    if(weather.temperature.value === undefined) return;
+    
+    if(weather.temperature.unit == "celsius"){
+        let fahrenheit = celsiusToFahrenheit(weather.temperature.value);
+        fahrenheit = Math.floor(fahrenheit);
+        
+        tempElement.innerHTML = `${fahrenheit}°<span>F</span>`;
+        weather.temperature.unit = "fahrenheit";
+    }else{
+        tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+        weather.temperature.unit = "celsius"
+    }
+});
